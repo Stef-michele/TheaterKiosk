176e4274eb42b9602c837bcc31481f8c08c6d4a4
Stefanie Gay <stefanie.gay@gmail.com>
2024-02-06 21:26:23 -0500

    Finished

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {//defining variables
+        int userAge = 0;
+        //output
+        System.out.println("Enter Your Age:");
+        //fake scanner input
+        userAge = 25;
+        if(userAge >=21){
+            System.out.println("You are 21+ You get a wristband!");
+        }
+
+
+    }
+}
